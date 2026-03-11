504c26473cb120a14df58eca24820696b4f27d81
dklora76-design <dklora76@gmail.com>
2026-03-11 16:18:15 +0400

    Add files via upload

---
+++ FullName.js
@@ -0,0 +1,6 @@
+function concatenation()
+{
+    firstName = document.getElementById("firstName").value
+    lastName = document.getElementById("lastName").value
+    document.getElementById("fullName").innerHTML="Full Name: " + firstName + " " + lastName;
+}
